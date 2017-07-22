83dddb88a7a578d65370923f774013c88250dfee
rodorian <karioster@mail.ru>
2017-07-22 22:50:02 +0300

    second commit

---
+++ blet.php
@@ -0,0 +1,44 @@
+<?php 
+$whatever = $_POST['whatever'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$user_message = $_POST['user_message'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'karioster@mail.ru';                 // SMTP username
+$mail->Password = 'r2gA1(qSZPwm';                           // SMTP password
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+
+$mail->setFrom('karioster@mail.ru', 'Ildar S. Zhiganshin');
+$mail->addAddress('karioster@mail.ru', 'Joe User');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Это сабджект сообщения';
+$mail->Body    = "
+ФОРМА: ".htmlspecialchars($whatever)."<br />
+Имя пользователя: ".htmlspecialchars($username)."<br />
+ПОЧТА: ".htmlspecialchars($email)."<br />
+текст: ".htmlspecialchars($user_message);
+$mail->AltBody = 'Это сообщение в формате plain text';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} 
+
+?>
